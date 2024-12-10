ca97ffa59061a8c354c64347e24a12863a2c46f7
abiudev <bdmasinde@gmail.com>
2024-12-10 10:06:54 +0300

    fixed .env api keys

--- src/App.jsx
+++ src/App.jsx
@@ -5,131 +5,128 @@ import TopShows from "./components/TopShows.jsx";
 import SpotifyShowCard from "./components/SpotifyShowCard.jsx";
 import { Routes, Route, useNavigate } from "react-router-dom";
 
-const CLIENT_ID =  import.meta.env.VITE_SPFY_API_KEY ;
-const CLIENT_SECRET = import.meta.env.VITE_SPFY_SECRET_KEY ;
+const CLIENT_ID = import.meta.env.VITE_SPFY_API_KEY;
+const CLIENT_SECRET = import.meta.env.VITE_SPFY_SECRET_KEY;
 
 export default function App() {
-	const [searchTerm, setSearchTerm] = useState("");
-	const [accessToken, setAccessToken] = useState(null);
-	const [shows, setShows] = useState([]);
-	const [suggestions, setSuggestions] = useState([]);
-	const [id, setId] = useState();
-	const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState("");
+  const [accessToken, setAccessToken] = useState(null);
+  const [shows, setShows] = useState([]);
+  const [suggestions, setSuggestions] = useState([]);
+  const [id, setId] = useState();
+  const navigate = useNavigate();
 
-	useEffect(() => {
-		const fetchAccessToken = async () => {
-			const options = {
-				method: "POST",
-				url: "https://accounts.spotify.com/api/token",
-				headers: {
-					"Content-Type": "application/x-www-form-urlencoded",
-				},
-				data: `grant_type=client_credentials&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`,
-			};
+  useEffect(() => {
+    const fetchAccessToken = async () => {
+      const options = {
+        method: "POST",
+        url: "https://accounts.spotify.com/api/token",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        data: `grant_type=client_credentials&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`,
+      };
 
-			try {
-				const response = await Axios(options);
-				setAccessToken(response.data.access_token);
-			} catch (error) {
-				console.error("Error fetching access token:", error);
-			}
-		};
+      try {
+        const response = await Axios(options);
+        setAccessToken(response.data.access_token);
+      } catch (error) {
+        console.error("Error fetching access token:", error);
+      }
+    };
 
-		fetchAccessToken();
-	}, []);
+    fetchAccessToken();
+  }, []);
 
-	const handleSearchChange = async (e) => {
-		const term = e.target.value;
-		setSearchTerm(term);
+  const handleSearchChange = async (e) => {
+    const term = e.target.value;
+    setSearchTerm(term);
 
-		if (!term) {
-			setSuggestions([]);
-			return;
-		}
+    if (!term) {
+      setSuggestions([]);
+      return;
+    }
 
-		try {
-			const { data } = await Axios.get("https://api.spotify.com/v1/search", {
-				headers: {
-					Authorization: `Bearer ${accessToken}`,
-				},
-				params: {
-					q: term,
-					type: "show",
-					market: "US",
-					limit: 5,
-				},
-			});
+    try {
+      const { data } = await Axios.get("https://api.spotify.com/v1/search", {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+        params: {
+          q: term,
+          type: "show",
+          market: "US",
+          limit: 5,
+        },
+      });
 
-			setSuggestions(data.shows.items);
-		} catch (error) {
-			console.error("Error fetching suggestions:", error);
-		}
-	};
+      setSuggestions(data.shows.items);
+    } catch (error) {
+      console.error("Error fetching suggestions:", error);
+    }
+  };
 
-	const handleSuggestionClick = (suggestion) => {
-		setSearchTerm(suggestion.name);
-		setSuggestions([]);
-		handleSearchClick();
-	};
+  const handleSuggestionClick = (suggestion) => {
+    setSearchTerm(suggestion.name);
+    setSuggestions([]);
+    handleSearchClick();
+  };
 
-	const handleSearchClick = async () => {
-		if (!searchTerm || !accessToken) return;
+  const handleSearchClick = async () => {
+    if (!searchTerm || !accessToken) return;
 
-		try {
-			const { data } = await Axios.get("https://api.spotify.com/v1/search", {
-				headers: {
-					Authorization: `Bearer ${accessToken}`,
-				},
-				params: {
-					q: searchTerm,
-					type: "show",
-					market: "US",
-				},
-			});
+    try {
+      const { data } = await Axios.get("https://api.spotify.com/v1/search", {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+        params: {
+          q: searchTerm,
+          type: "show",
+          market: "US",
+        },
+      });
 
-			setShows(data.shows.items);
-			isSpotify: true;
-			console.log(data.shows.items);
-			navigate("/results", {
-				state: { shows: data.shows.items },
-			});
-		} catch (error) {
-			console.error("Error fetching shows:", error);
-		}
-	};
+      setShows(data.shows.items);
+      isSpotify: true;
+      console.log(data.shows.items);
+      navigate("/results", {
+        state: { shows: data.shows.items },
+      });
+    } catch (error) {
+      console.error("Error fetching shows:", error);
+    }
+  };
 
-	useEffect(() => {
-		const ids = shows.map((show) => show.id);
-		setId(ids);
-		
-	}, [shows]);
+  useEffect(() => {
+    const ids = shows.map((show) => show.id);
+    setId(ids);
+  }, [shows]);
 
-	return (
-		
-			<div>
-				<Header
-					handleSearchChange={handleSearchChange}
-					handleSearchClick={handleSearchClick}
-					searchTerm={searchTerm}
-					suggestions={suggestions}
-					onSuggestionClick={handleSuggestionClick}
-				/>
-				<Routes>
-					<Route path="/trending" element={<TopShows />} />
+  return (
+    <div>
+      <Header
+        handleSearchChange={handleSearchChange}
+        handleSearchClick={handleSearchClick}
+        searchTerm={searchTerm}
+        suggestions={suggestions}
+        onSuggestionClick={handleSuggestionClick}
+      />
+      <Routes>
+        <Route path="/" element={<TopShows />} />
 
-					<Route
-						path="/results"
-						element={
-							<SpotifyShowCard
-								id={id}
-								accessToken={accessToken}
-								shows={shows}
-								isSpotify={true}
-							/>
-						}
-					/>
-				</Routes>
-			</div>
-		
-	);
+        <Route
+          path="/results"
+          element={
+            <SpotifyShowCard
+              id={id}
+              accessToken={accessToken}
+              shows={shows}
+              isSpotify={true}
+            />
+          }
+        />
+      </Routes>
+    </div>
+  );
 }
